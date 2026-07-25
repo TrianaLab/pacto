68b128379833d39bf03f0fd09211ec2258c07528
Eduardo Diaz <edudiazasencio@gmail.com>
2026-07-25 18:16:30 +0200

    chore(dashboard): rebuild UI bundle for evaluation coverage rendering

---
+++ pkg/dashboard/ui/assets/eventmodeling-FCH6USID-DziGt9Jb.js
@@ -0,0 +1,1 @@
+import"./chunk-NNHCCRGN-DlpIbxXb.js";import{y as e}from"./mermaid-parser.core-Cq8oDijf.js";export{e as createEventModelingServices};
---
+++ pkg/dashboard/ui/assets/gitGraph-WXDBUCRP-CuQ1WKg4.js
@@ -0,0 +1,1 @@
+import"./chunk-NNHCCRGN-DlpIbxXb.js";import{_ as e}from"./mermaid-parser.core-Cq8oDijf.js";export{e as createGitGraphServices};
